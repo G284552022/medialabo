060ac24ef456cba08eb5bc0581c380ffc89f680b
G284552022 <228455@st.takushoku-u.ac.jp>
2023-06-07 19:02:15 +0900

    プロジェクト課題

--- project.js
+++ project.js
@@ -1,40 +1,26 @@
 
-let KID;
 let ID ={
-    id: [360630,524901,993800] 
+    id: [360630,524901,993800,1816670,1850147,1880252,2147714,2643743,2968815,3451189,5128581,5368361] 
 };
-KID = ID.id[0];
-//for (let n of ID.id) {
-  //      console.log(n);
-//}    
-
-
-/*    let b = document.querySelector('button#btn');
-b.addEventListener('click', showSelectResult);
-
-function showSelectResult() {
-    let s = document.querySelector('select#kuni');
-    let idx = s.selectedIndex;
-    let os = s.querySelectorAll('option');  
-    let o = os.item(idx);      
-
-    console.log('  value=' + o.getAttribute('value'));
-    console.log('  textContent='+o.textContent);
-    */
-    
     let b = document.querySelector('#sendRequest');
     b.addEventListener('click', sendRequest);
 
 function sendRequest() {
-    
+
+    //let KID;
+
+//KID = ID.id[0];
+for (let id of ID.id) {
+
     // URL を設定
-    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/360630.json';
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id +'.json';
 
     // 通信開始
     axios.get(url)
         .then(showResult)   // 通信成功
         .catch(showError)   // 通信失敗
         .then(finish);      // 通信の最後の処理
+    }
 }
 // 通信が成功した時の処理
 function showResult(resp) {
@@ -46,19 +32,51 @@ function showResult(resp) {
         data = JSON.parse(data);
     }
 
+ let B = document.querySelector('button#btn');
+B.addEventListener('click', showSelectResult);
 
+function showSelectResult() {
+    let s = document.querySelector('select#kuni');
+    let idx = s.selectedIndex;
+    let os = s.querySelectorAll('option');  
+    let o = os.item(idx);      
 
+    console.log('  textContent='+o.textContent);
+    
+    if(o.textContent == data.name){
+
+    
 
 
 
     // data をコンソールに出力
-    console.log(data);
+    //console.log(data);
 
     // data.x を出力
     console.log(data.x);
 
-
+    let An = document.querySelector('span#answer');		
+        An.textContent = "都市名  "+data.name;
+        let Bn = document.querySelector('span#B');		
+        Bn.textContent = "天気  "+data.weather[0].description;
+        let Cn = document.querySelector('span#C');		
+        Cn.textContent = "最低気温  "+data.main.temp_min +"度";
+        let Dn = document.querySelector('span#D');		
+        Dn.textContent = "最高気温  "+data.main.temp_max +"度";
+        let En = document.querySelector('span#E');		
+        En.textContent = "湿度  "+data.main.humidity;
+        let Fn = document.querySelector('span#F');		
+        Fn.textContent = "風速  "+data.wind.speed;
+        let Gn = document.querySelector('span#G');		
+        Gn.textContent = "風向  "+data.wind.deg;
+        let Hn = document.querySelector('span#H');		
+        Hn.textContent = "緯度  "+data.coord.lon +"度";
+        let In = document.querySelector('span#I');		
+        In.textContent = "経度  "+data.coord.lat +"度";
+    }
 }
+}
+
 
 
 
@@ -78,3 +96,10 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
+
+
+
+
+
+   
+    
